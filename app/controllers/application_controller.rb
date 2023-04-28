673798a338cf15b2dc3c3e782548a344c8fd4ef1
Cody <codybarker.or@gmail.com>
2023-04-28 10:46:54 -0700

    s

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  set :default_content_type, 'application/json'
+
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+  
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    items = BakedGood.all.order(price: :desc)
+    items.to_json
+  end
+
+  get 'baked_goods/most_expensive' do
+    item = BakedGood.all.order(price: :desc).limit(1)
+    item.to_json
+  end
 
 end
